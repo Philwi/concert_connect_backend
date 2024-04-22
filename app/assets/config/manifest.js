ba0e24c7ddee66918c0e847780a5f9562190057b
Philipp Winkler <philrigid@gmail.com>
2024-04-22 15:18:46 +0200

    feat: PoC for styling and concert index

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,9 +1,4 @@
 //= link_tree ../builds
 //= link_tree ../images
 //
-//= link_directory ../stylesheets/sprockets
-//
 //= link_directory ../../../vendor/javascript
-//= link_directory ../../javascript/importmap
-//
-//= link sprockets/application.js
